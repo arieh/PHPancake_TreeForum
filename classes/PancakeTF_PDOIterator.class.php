08a5af732be76f3443e89bec5bedd44082df7904
Arieh <arieh.glazer@gmail.com>
2009-11-25 09:59:16 +0200

    improved the iterator- unless count is called before 1st iteration is finished, the array will be pulled only at iteration instead of running it before

--- classes/PancakeTF_PDOIterator.class.php
+++ classes/PancakeTF_PDOIterator.class.php
@@ -1,24 +1,64 @@
 <?php
 
 class PancakeTF_PDOIterator implements Iterator,Countable{
-	private $current = null;
+	private $current_row = null;
 	private $position = 0;
-	private $array = array();
-	private $row_count = 0;
+	private $stack = array();
+	private $stms = null;
+	private $from_stack = false;
+	private $row_count = false;
     
     public function __construct(PDOStatement $stmt){
-    	$this->array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    	$this->row_count = count($this->array);
+    	$this->current_row = $stmt->fetch(PDO::FETCH_ASSOC);
+    	$this->stmt = $stmt;
     }
     
-   public function count(){return $this->row_count; }
-   public function key(){return $this->position;}
-   public function current(){return $this->array[$this->position];}
-   public function next(){
-   		$this->position++;
-   		if ($this->valid()) return $this->current;
-   		return false;
+   public function count(){
+		if (false === $this->row_count){
+			$this->stack[]=$this->current();
+			$this->stack = array_merge($this->stack,$this->stmt->fetchAll(PDO::FETCH_ASSOC));
+			$this->from_stack = true;
+			$this->row_count = count($this->stack);	
+		}
+		return $this->row_count;
    }
-   public function valid(){return $this->position<$this->row_count;}
-   public function rewind(){$this->position = 0;}
+   
+   public function key(){return $this->position;}
+   public function current(){return $this->current_row;}
+   public function next(){
+   		if ($this->from_stack){
+   			$this->position++;
+   			if ($this->valid())
+   				$this->current_row = $this->stack[$this->position];
+   			return;
+   		}
+		$this->stack[]=$this->current_row;
+   		$this->position++; 
+   		$this->current_row = $this->stmt->fetch(PDO::FETCH_ASSOC);
+   		
+   		if (false === $this->valid()){
+   			$this->from_stack = true;
+   			$this->row_count = count($this->stack);
+   			return;
+   		}
+   }
+   
+   public function valid(){
+		if ($this->from_stack) return ($this->position<$this->count());
+		return (bool)$this->current_row;
+   }
+   
+   public function rewind(){
+   		if ($this->position===0) return;
+   		if ($this->valid()){
+   			$this->stack = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   			$this->row_count = count($this->stack);
+   			$this->from_stack = true;
+   		}
+   		$this->position = 0;
+   		if ($this->from_stack){
+   			$this->current_row = $this->stack[0];
+   		}
+   		
+   }
 }
